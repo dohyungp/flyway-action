ebdbd6d02732dbf24ef74a1d1a30cd8ef1016d52
Dohyung Park <dohyung.prk@gmail.com>
2024-06-06 02:13:14 +0900

    Make info result to step summary

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,4 @@
 set -e
 
 /flyway/flyway migrate
-/flyway/flyway info
-# result=$(/flyway/flyway info)
-echo "migration-status=success" >> $GITHUB_OUTPUT
+/flyway/flyway info >> $GITHUB_STEP_SUMMARY
